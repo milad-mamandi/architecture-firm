3153600811747611d9e91d3673fc0eec6d14448e
milad mamandi <43762677+milad-mamandi@users.noreply.github.com>
2025-09-21 18:20:55 +0330

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ import AnimatedButton from '@/components/animated-button'
 
 export default function NotFound() {
   return (
-    <div className="flex h-[calc(100vh-(--spacing(18)))] flex-col items-center justify-center gap-8">
+    <div className="flex h-[calc(100dvh-(--spacing(18)))] flex-col items-center justify-center gap-8">
       <h1 className="text-7xl">404</h1>
       <p className="max-w-lg px-4 text-center md:text-lg">
         Looks like the page you're looking for isn't here. Maybe it’s taking a
